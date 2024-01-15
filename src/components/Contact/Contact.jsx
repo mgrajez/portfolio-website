3c6df529e40464b8738ba3dfab9db9e904e38588
mgrajez <monikagrajez@gmail.com>
2024-01-15 16:28:16 +0100

    Implementing contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,30 @@
 import "./Contact.css";
+import { useState } from "react";
 
 function Contact() {
   return (
     <>
-      <h1 className="contact-title">Contact</h1>
+      <section className="contactPage">
+        <div id="contact">
+          <h1 className="contactPageTitle">Contact</h1>
+          <span className="contactDesc">
+            Please fill out the form below to discuss any work opportunities.
+          </span>
+          <form action="" className="contactForm">
+            <input type="text" className="name" placeholder="Your name" />
+            <input type="text" className="email" placeholder="Your email" />
+            <textarea
+              className="msg"
+              name="message"
+              rows="5"
+              placeholder="Your message"
+            ></textarea>
+            <button type="submit" value="Send" className="submitBtn">
+              Submit
+            </button>
+          </form>
+        </div>
+      </section>
     </>
   );
 }
