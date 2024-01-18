d152f17c6fdae8a14787675d55894f9c3e53c79e
mgrajez <monikagrajez@gmail.com>
2024-01-18 15:25:53 +0100

    Implementation of EmailJS. Contact form completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,37 @@
 import "./Contact.css";
-import { useState } from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
+  const form = useRef();
+  const [message, setMessage] = useState(null);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_i6dfo1e",
+        "template_m1z39x5",
+        form.current,
+        "uoJdRZTh-9UB2c4Le"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          e.target.reset();
+          setMessage("Message sent. Thank you!");
+
+          setTimeout(() => {
+            setMessage(null);
+          }, 5000);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <>
       <section className="contactPage">
@@ -10,19 +40,33 @@ function Contact() {
           <span className="contactDesc">
             Please fill out the form below to discuss any work opportunities.
           </span>
-          <form action="" className="contactForm">
-            <input type="text" className="name" placeholder="Your name" />
-            <input type="text" className="email" placeholder="Your email" />
+          <form className="contactForm" ref={form} onSubmit={sendEmail}>
+            <input
+              type="text"
+              className="name"
+              placeholder="Your name"
+              name="your_name"
+              required
+            />
+            <input
+              type="email"
+              className="email"
+              placeholder="Your email"
+              name="your_email"
+              required
+            />
             <textarea
               className="msg"
               name="message"
               rows="5"
               placeholder="Your message"
+              required
             ></textarea>
             <button type="submit" value="Send" className="submitBtn">
               Submit
             </button>
           </form>
+          {message && <p className="success-message">{message}</p>}
         </div>
       </section>
     </>
