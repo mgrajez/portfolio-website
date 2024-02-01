1ac0b8fa7df0d8dedbba523d6e081c953e26018e
mgrajez <monikagrajez@gmail.com>
2024-02-01 22:02:22 +0100

    Fixes to hero section -  responsiveness

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,20 +8,22 @@ function Hero() {
   return (
     <>
       <section className="container-hero" id="home">
-        <img className="hero-image" src={heroImage} alt="hero image" />
-        <div className="content">
-          <h1 className="title">
-            Hello, <br />I am <span className="title-name">Monika</span>
-          </h1>
-          <p className="job-title">Frontend Web Developer</p>
-          <p className="description">
-            I'm passionate about crafting <br /> websites that are engaging,{" "}
-            <br />
-            functional, and user-centric.
-          </p>
-          <Link to="contact">
-            <button className="contact-button">Get in touch</button>
-          </Link>
+        <div className="content-container">
+          <img className="hero-image" src={heroImage} alt="hero image" />
+          <div className="content">
+            <h1 className="title">
+              Hello, <br />I am <span className="title-name">Monika</span>
+            </h1>
+            <p className="job-title">Frontend Web Developer</p>
+            <p className="description">
+              I'm passionate about crafting <br /> websites that are engaging,{" "}
+              <br />
+              functional, and user-centric.
+            </p>
+            <Link to="contact">
+              <button className="contact-button">Get in touch</button>
+            </Link>
+          </div>
         </div>
         <Link to="about">
           <div className="scroll-icon">
